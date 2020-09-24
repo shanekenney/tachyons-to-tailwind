73bebd4ae7a129adf4f58490caafc9e36c01d615
Shane Kenney <me@shanek.org>
2020-09-24 12:37:33 +1000

    Read files supplied as cli args

--- Main.hs
+++ Main.hs
@@ -1,23 +1,51 @@
 module Main where
 
+import Control.Monad
 import Data.Function ((&))
 import Data.List (intercalate)
 import Data.Text as Text (Text, unpack)
+import qualified Data.Text.IO as Text.IO
+import Options.Applicative
 import Tachyons (NestedBlock, classes, parse)
 
+-------------------------------------------------------------------------------
+-- Options Parsing
+-------------------------------------------------------------------------------
+
+data Opts = Opts [FilePath]
+  deriving (Show)
+
+optsParser :: Parser Opts
+optsParser = Opts <$> some (argument str (metavar "FILES..."))
+
+opts :: ParserInfo Opts
+opts = info (optsParser <**> helper) idm
+
+-------------------------------------------------------------------------------
+-- Main
+-------------------------------------------------------------------------------
+
 main :: IO ()
-main = do
+main = execParser opts >>= run
+
+run :: Opts -> IO ()
+run (Opts files) = do
   let dataFile = "tachyons.css"
+  readAndDisplayFiles files
   result <- parse dataFile
   either (printParseError dataFile) processTachyons result
+
+processTachyons :: [NestedBlock] -> IO ()
+processTachyons = printClasses . classes
+
+-------------------------------------------------------------------------------
+-- Display
+-------------------------------------------------------------------------------
 
 printParseError :: String -> String -> IO ()
 printParseError dataFile err = do
   print $ "Failed parsing " ++ dataFile
   print err
-
-processTachyons :: [NestedBlock] -> IO ()
-processTachyons = printClasses . classes
 
 printClasses :: [Text] -> IO ()
 printClasses classes' =
@@ -25,3 +53,6 @@ printClasses classes' =
     & map unpack
     & intercalate "\n"
     & putStrLn
+
+readAndDisplayFiles :: [FilePath] -> IO ()
+readAndDisplayFiles = mapM_ $ Text.IO.readFile >=> Text.IO.putStrLn
